08922a63798f0a16cfa05aae93b8b1a466e3c01f
sharibarboza <barbozashari@gmail.com>
2019-04-17 15:22:41 -0600

    Add preview button for files

--- src/components/filedownload.js
+++ src/components/filedownload.js
@@ -14,14 +14,16 @@ class FileDownload extends React.Component {
         <div className="col-lg-1 col-md-1 hidden-xs-down">
           <FileIcon media={bylaw.file.internal.mediaType} />
         </div>
-        <div className="col-lg-8 col-md-7 col-xs-12">
+        <div className="col-lg-6 col-md-7 col-xs-12">
           <strong>{bylaw.title}</strong><br />
           <span>{bylaw.file.relativePath}</span>
         </div>
-        <div className="col-lg-3 col-md-4 col-xs-12" style={{
+        <div className="col-lg-5 col-md-4 col-xs-12" style={{
           textAlign: 'right',
           paddingTop: '5px'
         }}>
+          <a href={bylaw.file.publicURL} target="_blank"><div className="btn download-btn"><i className="fa fa-search-plus"></i> Preview</div></a>
+          &nbsp;&nbsp;
           <a href={bylaw.file.publicURL} download><div className="btn download-btn"><i className="fa fa-download"></i> Download</div></a>
         </div>
       </div>
