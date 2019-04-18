2857ec7236f6231994cf9a6240719e27ae534307
sharibarboza <barbozashari@gmail.com>
2019-04-18 17:33:06 -0600

    Add hidden date to reports files

--- src/components/filedownload.js
+++ src/components/filedownload.js
@@ -15,15 +15,13 @@ class FileDownload extends React.Component {
           <FileIcon media={bylaw.file.internal.mediaType} />
         </div>
         <div className="col-lg-6 col-md-7 col-xs-12">
-          <strong>{bylaw.title}</strong><br />
+          <strong><a href={bylaw.file.publicURL} target="_blank">{bylaw.title}</a></strong><br />
           <span>{bylaw.file.relativePath}</span>
         </div>
         <div className="col-lg-5 col-md-4 col-xs-12" style={{
           textAlign: 'right',
           paddingTop: '5px'
         }}>
-          <a href={bylaw.file.publicURL} target="_blank"><div className="btn download-btn"><i className="fa fa-search-plus"></i> Preview</div></a>
-          &nbsp;&nbsp;
           <a href={bylaw.file.publicURL} download><div className="btn download-btn"><i className="fa fa-download"></i> Download</div></a>
         </div>
       </div>
