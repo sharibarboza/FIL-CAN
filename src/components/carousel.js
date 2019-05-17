3e4693beb8e1d7b4bb585e586a8bdfe51d5140b6
sharibarboza <barbozashari@gmail.com>
2019-05-17 17:45:30 -0600

    Fix carousel button duration to 2s

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -39,7 +39,7 @@ class Slider extends React.Component {
         <div className="wow slideInRight" data-wow-duration="2s" data-wow-delay="0s">
           <p  className="em-slider-descript">Welcome to the Filipino-Canadian Seventh-Day Adventist Association of Alberta</p>
         </div>
-        <div className="em-slider-button wow  bounceInUp  em-button-button-area" data-wow-duration="1s" data-wow-delay="0s">
+        <div className="em-slider-button wow  bounceInUp  em-button-button-area" data-wow-duration="2s" data-wow-delay="0s">
           <Link to="/campmeeting/">Annual camp meeting</Link>
         </div>
       </div>
