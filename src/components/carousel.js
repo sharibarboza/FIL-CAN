17e7843731a22013879a28398dfda770695a556d
sharibarboza <barbozashari@gmail.com>
2019-05-17 17:38:03 -0600

    Fix carousel image button

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -36,10 +36,10 @@ class Slider extends React.Component {
         <div className="wow slideInRight" data-wow-duration="2s" data-wow-delay="0s">
           <h1 className="em-slider-sub-title">Uniting Filipino-Canadians </h1>
         </div>
-        <div className="wow slideInRight" data-wow-duration="3s" data-wow-delay="0s">
+        <div className="wow slideInRight" data-wow-duration="2s" data-wow-delay="0s">
           <p  className="em-slider-descript">Welcome to the Filipino-Canadian Seventh-Day Adventist Association of Alberta</p>
         </div>
-        <div className="em-slider-button wow  bounceInUp  em-button-button-area" data-wow-duration="3s" data-wow-delay="0s">
+        <div className="em-slider-button wow  bounceInUp  em-button-button-area" data-wow-duration="1s" data-wow-delay="0s">
           <Link to="/campmeeting/">Annual camp meeting</Link>
         </div>
       </div>
