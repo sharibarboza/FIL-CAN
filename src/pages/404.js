2094e1ac9fa48e31af9a293a66929e9a023b5278
sharibarboza <barbozashari@gmail.com>
2019-06-20 23:31:42 -0600

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const NotFoundPage = () => (
-  <div>
+  <div align="center" style={{
+    textAlign: 'center',
+    padding: '50px'
+  }}>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
