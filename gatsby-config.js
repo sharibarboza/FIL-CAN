5ee64ad242fd6977c962ead1a10086587102ccad
sharibarboza <barbozashari@gmail.com>
2019-06-21 02:03:25 -0600

    Fix config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-sitemap',
     {
       resolve: `gatsby-transformer-remark`,
       options: {
