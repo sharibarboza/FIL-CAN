d8c22c6081b4973daa141e0e296ffde1da6d0814
sharibarboza <barbozashari@gmail.com>
2022-03-13 13:12:22 -0600

    Add website link to README.md

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -12,42 +12,42 @@ class Youtube extends React.Component {
   }
 
   render() {
-  	return (
-  	<div className="container area-padding">
-  		<div className="row">
-  			<div className="col-md-12">
+    return (
+    <div className="container area-padding">
+      <div className="row">
+        <div className="col-md-12">
           <div className="section-title t_center">
 
             <h2 style={{
-            	color: '#ffffff'
+              color: '#ffffff'
             }}>Videos & Livestreams</h2>
             <Divider />
           </div>
-  			</div>
-  		</div>
+        </div>
+      </div>
 
       <div><Video channel={this.channel} /></div>
 
       <div className="row">
-      	<div style={{
-      		display: 'block',
-      		margin: '0 auto',
-      		textAlign: 'center'
-      	}}>
+        <div style={{
+          display: 'block',
+          margin: '0 auto',
+          textAlign: 'center'
+        }}>
 
-    			<div className="row">
-    				<div className="col-md-12">
-    					<div className="section-title t_center">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="section-title t_center">
                 <div className="donate-btn-header">
                   <a href="https://www.youtube.com/channel/UCRjkITPyt10LSzbOs5hojUw" target="_blank" className="red-on-white">View More Videos</a>
                 </div>
-    					</div>
-    				</div>
-    			</div>
-      	</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-  	</div>
-  	)
+    </div>
+    )
   }
 }
 
